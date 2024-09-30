9e72077244fe8eceeab0600af9e35af9577d0482
DugnCon <damdaidung9b@gmail.com>
2024-09-30 16:45:32 +0700

    update new commit

---
+++ Manager/src/Person.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    private String Name;
+    private String Date;
+    public Person(String Name, String Date){
+        this.Name = Name;
+        this.Date = Date;
+    }
+    void chuanHoa(){
+        StringBuilder S = new StringBuilder("");
+        String[] arr = new String[];
+        String name = Name.split
+    }
+}
